09f384488e4b90fadff8aab1037c238847ab113e
NgTuanLoc <tuanloc2352000@gmail.com>
2022-08-22 20:32:21 +0700

    third setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import './app.css';
+
+function App() {
+	return <h1 className='underline text-3xl'>APP</h1>;
+}
+
+export default App;
--- src/features/adminSlice.js
+++ src/features/adminSlice.js
@@ -1,14 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	data: '',
+	screenSize: undefined,
+	currentColor: '#03C9D7',
+	currentMode: 'Light',
+	themeSettings: false,
+	activeMenu: true,
+	isClicked: {
+		chat: false,
+		cart: false,
+		userProfile: false,
+		notification: false,
+	},
 };
 
 const adminSlice = createSlice({
 	name: 'admin',
 	initialState,
 	reducers: {
-		test: {},
+		setMode: (state, { payload }) => {
+			state.currentMode = payload;
+			localStorage.setItem('themeMode', payload);
+		},
+		setColor: (state, { payload }) => {
+			state.currentColor(payload);
+			localStorage.setItem('colorMode', payload);
+		},
+		handleClick: (state, { payload }) => {
+			state.isClicked = {
+				...state.isClicked,
+				[payload]: true,
+			};
+		},
 	},
 });
 
