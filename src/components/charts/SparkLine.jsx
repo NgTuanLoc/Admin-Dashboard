15e56ffe3ccd9f6a5c85af81b944286eec3f0312
NgTuanLoc <tuanloc2352000@gmail.com>
2022-08-28 10:36:03 +0700

    update

--- src/components/charts/SparkLine.jsx
+++ src/components/charts/SparkLine.jsx
@@ -1,5 +1,26 @@
-const SparkLine = () => {
-	return <div>SparkLine</div>;
+import {
+	SparklineComponent,
+	Inject,
+	SparklineTooltip,
+} from '@syncfusion/ej2-react-charts';
+
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+	return (
+		<SparklineComponent
+			id={id}
+			height={height}
+			width={width}
+			lineWidth={3}
+			valueType='Numeric'
+			fill={color}
+			border={{ color: currentColor, width: 2 }}
+			dataSource={data}
+			type={type}
+			xName='x'
+			yName='y'>
+			<Inject services={[SparklineTooltip]} />
+		</SparklineComponent>
+	);
 };
 
 export default SparkLine;
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,7 +6,7 @@ import {
 	SparklineAreaData,
 	ecomPieChartData,
 } from '../data/dummy';
-import { Button } from '../components';
+import { Button, SparkLine } from '../components';
 
 const Ecommerce = () => {
 	return (
@@ -100,6 +100,17 @@ const Ecommerce = () => {
 									<span className='text-3xl font-semibold'>$48,192</span>
 								</p>
 								<p className='text-gray-500 mt-1'>Expense</p>
+							</div>
+							<div className='mt-5'>
+								<SparkLine
+									currentColor='blue'
+									id='line-sparkline'
+									type='Line'
+									height='80px'
+									width='250px'
+									data={SparklineAreaData}
+									color='blue'
+								/>
 							</div>
 						</div>
 					</div>
