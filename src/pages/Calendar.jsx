fcff134acc69d149b847983af90e7cc9f4cec0c8
NgTuanLoc <tuanloc2352000@gmail.com>
2022-09-02 20:43:13 +0700

    setup calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,35 @@
+import {
+	ScheduleComponent,
+	ViewDirective,
+	ViewsDirective,
+	Day,
+	Week,
+	WorkWeek,
+	Month,
+	Agenda,
+	Inject,
+	Resize,
+	DragAndDrop,
+} from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+
+import { scheduleData } from '../data/dummy';
+import { Header } from '../components';
+
 const Calendar = () => {
-	return <div>Calendar</div>;
+	return (
+		<div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+			<Header category='App' title='Calendar' />
+			<ScheduleComponent
+				height='650px'
+				eventSettings={{ dataSource: scheduleData }}>
+				<Inject
+					services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+					selectedData={new Date(2021, 0, 10)}
+				/>
+			</ScheduleComponent>
+		</div>
+	);
 };
 
 export default Calendar;
